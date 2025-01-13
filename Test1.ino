b0344f207d843cfc6eb5bac5be1819d02952d676
Hans Olav <hansolavaarvik@gmail.com>
2025-01-13 14:11:49 +0100

    liten oppdatering

--- Test1.ino
+++ Test1.ino
@@ -1,3 +1,4 @@
+#include <avr/io.h>
 // #include <avr/io.h>
 // //Pot reads in range [0,633]
 // float duration, distance;
@@ -32,43 +33,52 @@
 
 //   delay(1000);
 // }
-#include <avr/io.h>
+
 
 // Global variables for duration and distance
-float duration, distance;
-
+float duration, distance, duration_two;
+int trig = 3;
+int echo = 2;
+int out  = 0;
 void setup() {
   // Initialize serial communication at 9600 baud
   Serial.begin(9600);
   
   // Set PB1 (Trig) as output and PB2 (Echo) as input
-  DDRB |= (1 << PB1);  // PB1 is output (Trig)
-  DDRB &= ~(1 << PB2); // PB2 is input (Echo)
+  DDRD |= (1 << trig);  // PB1 is output (Trig)
+  DDRD &= ~(1 << echo); // PB2 is input (Echo)
+  DDRB &= ~(1 << out);
 }
 
 void loop() {
   // Ultrasonic sensor reading
   
   // Make sure Trig is low for 2 microseconds
-  PORTB &= ~(1 << PB1);
+  PORTB &= ~(1 << trig);
   delayMicroseconds(2);
   
-  // Send a 10-microsecond HIGH pulse to the Trig pin (PB1)
-  PORTB |= (1 << PB1);
+  // Send a 10-microsecond HIGH pulse to the Trig pin (trig)
+  PORTB |= (1 << trig);
   delayMicroseconds(10);
-  PORTB &= ~(1 << PB1);  // Set Trig low after pulse
+  PORTB &= ~(1 << trig);  // Set Trig low after pulse
   
   // Measure the duration of the pulse on Echo pin (PB2)
-  duration = pulseIn(PB2, HIGH);
-  
+  //duration = pulseIn(echo, HIGH);
+  duration_two = pulseIn(out, HIGH);
   // Calculate distance in cm
-  distance = (duration * 0.0343) / 2;
+  distance = duration * 0.0343 / 2;
   
   // Print the distance
+  
+  Serial.print("duration: ");
+  Serial.print(duration_two);
+  Serial.println(" ms");
+
   Serial.print("Distance: ");
   Serial.print(distance);
   Serial.println(" cm");
-  
+
+
   // Wait 1 second before taking another measurement
-  delay(100);
+  delay(500);
 }
