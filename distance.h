61debd43e03b89ecab45ec26b7727f242924e6c3
Hans Olav <hansolavaarvik@gmail.com>
2025-01-13 14:43:47 +0100

    tester å dele opp i filer

---
+++ distance.h
@@ -0,0 +1,18 @@
+#include <avr/io.h>
+// Global variables for duration and distance
+float duration, distance;
+float distance_calc();
+// pins
+int trig = 3;
+int echo = 2;
+int out = 0;
+
+float distance_calc();
+  PORTD &= ~(1 << trig);// Make sure Trig is low for 2 microseconds
+  delayMicroseconds(2);
+  PORTD |= (1 << trig);// Send a 10-microsecond HIGH pulse to the Trig pin (trig)
+  delayMicroseconds(10);
+  PORTD &= ~(1 << trig);  // Set Trig low after pulse
+  duration = pulseIn(out, HIGH);
+  distance = duration * 0.0343 / 2;  // Calculate distance in cm
+  return distance;
---
+++ main.ino
@@ -0,0 +1,23 @@
+#include <avr/io.h>
+#include <distance.h>
+
+void setup() {
+  Serial.begin(9600);
+  
+  // ulstrasonic
+  DDRD |= (1 << trig);  // PD1 is output (Trig)
+  DDRD &= ~(1 << echo); // PD2 is input (Echo)
+  DDRB &= ~(1 << out);
+}
+
+void loop() {
+
+  distance_calc();
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+
+
+  // Wait 1 second before taking another measurement
+  delay(500);
+}
