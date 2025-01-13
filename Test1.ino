3da5681b9debad1f98c2bb6da57455cf06435be1
Hans Olav <hansolavaarvik@gmail.com>
2025-01-13 14:21:23 +0100

    litt hjelp frå claude

--- Test1.ino
+++ Test1.ino
@@ -39,12 +39,13 @@
 float duration, distance, duration_two;
 int trig = 3;
 int echo = 2;
-int out  = 0;
+int out = 0;
+
 void setup() {
   // Initialize serial communication at 9600 baud
   Serial.begin(9600);
   
-  // Set PB1 (Trig) as output and PB2 (Echo) as input
+  // Set PD3 (Trig) as output and PD2 (Echo) as input
   DDRD |= (1 << trig);  // PB1 is output (Trig)
   DDRD &= ~(1 << echo); // PB2 is input (Echo)
   DDRB &= ~(1 << out);
@@ -54,13 +55,13 @@ void loop() {
   // Ultrasonic sensor reading
   
   // Make sure Trig is low for 2 microseconds
-  PORTB &= ~(1 << trig);
+  PORTD &= ~(1 << trig);
   delayMicroseconds(2);
   
   // Send a 10-microsecond HIGH pulse to the Trig pin (trig)
-  PORTB |= (1 << trig);
+  PORTD |= (1 << trig);
   delayMicroseconds(10);
-  PORTB &= ~(1 << trig);  // Set Trig low after pulse
+  PORTD &= ~(1 << trig);  // Set Trig low after pulse
   
   // Measure the duration of the pulse on Echo pin (PB2)
   //duration = pulseIn(echo, HIGH);
@@ -69,11 +70,6 @@ void loop() {
   distance = duration * 0.0343 / 2;
   
   // Print the distance
-  
-  Serial.print("duration: ");
-  Serial.print(duration_two);
-  Serial.println(" ms");
-
   Serial.print("Distance: ");
   Serial.print(distance);
   Serial.println(" cm");
